afa38b1813bd87c30f692aeeb45a14067e41baa2
Yuna Nomura <nomurayu88@craighead.school.nz>
2024-08-21 04:16:29 +0000

    I used true and break to play and stop until the user's guess was the same as the computer's number, and it worked. And I was also able to correctly display the result after the user's final attempt.

---
+++ script.js
@@ -0,0 +1,26 @@
+function game() {
+    alert("Let's play Guess the Number!")
+    let name = prompt('What is your name?')
+    //get the user's name
+    alert('Hi, ' + name + "! I am thinking of a number between 1 and 20 inclusive.")
+//output with using the name that I received
+    let computerNumber = Math.floor(Math.random() * (20 - 1) + 1)
+//A number between 1 and 20 will be chosen randomly
+
+    let count = 0
+    while (true) {
+        count = count + 1
+
+        let userAttempt = prompt('Enter your guess.')
+        if (userAttempt == computerNumber) {
+            alert('Well done!')
+            break;
+        } else {
+            alert('Nope!')
+        }
+    }
+    result()
+}
+function result() {
+    alert('Your score is ' + count + '!')
+}
