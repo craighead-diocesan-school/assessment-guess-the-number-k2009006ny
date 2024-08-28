fde045fb717e05df960f6ab63d8d9461c22b6a85
Yuna Nomura <nomurayu88@craighead.school.nz>
2024-08-28 03:26:07 +0000

    I added scores.push(result) and it worked.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function game() {
     alert("Let's play Guess the Number!")
-    let name = prompt('What is your name?')
+    let userName = prompt('What is your name?')
     //get the user's name
-    alert('Hi, ' + name + "! I am thinking of a number between 1 and 20 inclusive.")
+    alert('Hi, ' + userName + "! I am thinking of a number between 1 and 20 inclusive.")
     //output with using the name that I received
     let computerNumber = Math.floor(Math.random() * (20 - 1) + 1)
     //A number between 1 and 20 will be chosen randomly
@@ -14,21 +14,18 @@ function game() {
         let userAttempt = prompt('Enter your guess.')
         if (userAttempt == computerNumber) {
             alert('Well done!')
+            let result = (userName + ',' + count)
+            // newScore = userName + ',' + count
+            scores.push(result)
             break;
         } else {
             alert('Nope!')
         }
     }
-    addNewScore()
+
 }
-function addNewScore() {
-    scores.push("Yuna, 3")
-}
-let scores = [
-    "Yuna, 1",
-    "Yuna, 5",
-    "Yuna, 7"
-]
+
+let scores = []
 
 function result() {
     alert(scores)
